f2becc2cbc11d71b922d4da418abb8ae0e0a4c46
dna-lattex <71548825+dna-lattex@users.noreply.github.com>
2021-01-02 21:31:58 +0700

    init

--- myweb/myweb_content/wflogs/config.php
+++
@@ -1,6 +0,0 @@
-<?php exit('Access denied'); __halt_compiler(); ?>
-******************************************************************
-This file is used by the Wordfence Web Application Firewall. Read 
-more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
-******************************************************************
-a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"-oY7wN&,,Y^hvPd00c77FPlU) 4  !=lD(,x5.<}yo0)(t7Q,Dp,2=Z%L`BKt_9Y";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:2545;}
